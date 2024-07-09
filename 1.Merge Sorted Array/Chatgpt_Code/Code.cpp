5770c343fd5d56e76616fd54435a44499d2a24ae
sanjid <58500573+sanjid-007@users.noreply.github.com>
2024-07-09 20:02:49 +0600

    Create Code.cpp

---
+++ 1.Merge Sorted Array/Chatgpt_Code/Code.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;  // Pointer for nums1
+        int j = n - 1;  // Pointer for nums2
+        int k = m + n - 1;  // Pointer for merged array in nums1
+
+        // Merge in reverse order
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // If there are remaining elements in nums2
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+
+        // No need to handle the remaining elements in nums1 as they are already in place
+    }
+};
+
